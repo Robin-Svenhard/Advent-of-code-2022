181ee850092ede399af63289d1f9cc04a7476f59
Robin Svenhard <robin.svenhard@evry.com>
2020-12-05 18:25:06 +0100

    gitignore

--- src/day04.kt
+++ src/day04.kt
@@ -1,5 +1,49 @@
+import java.lang.StringBuilder
+
+class Passport(val fields: Map<String, String>)
+
 fun day04(input: List<String>): Int {
 
+    var passportTemplate = StringBuilder()
+    val passports: MutableList<Passport> = mutableListOf()
 
-    return 0
+    for (s in input) {
+        if (s.length == 0) {
+            //create passport and reset template
+            passports.add(createPassport(passportTemplate))
+            passportTemplate = StringBuilder()
+        } else {
+            passportTemplate.append(s + " ")
+        }
+    }
+    passports.add(createPassport(passportTemplate))
+    return validatePassports(passports)
 }
+
+fun validatePassports(passports: List<Passport>): Int {
+    var count = 0
+    for (passport in passports){
+        val fields = passport.fields
+        if(fields.count() >= 7){
+            if(fields.count() == 8 || !fields.containsKey("cid")){
+                count++
+            }
+        }
+    }
+    return count
+}
+
+fun createPassport(template: StringBuilder): Passport {
+    val passport: MutableMap<String, String> = mutableMapOf()
+    val templateString = template.toString()
+    val fields = templateString.split(" ")
+    for (f in fields) {
+        if (f.length != 0) {
+            val field = f.split(":")
+            passport.put(field[0], field[1])
+        }
+    }
+    println(passport)
+    return Passport(passport)
+}
+
