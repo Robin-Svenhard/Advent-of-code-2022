d5c915f7828dfe690a9c9eb6df2845c91d404172
Robin Svenhard <robin.svenhard@evry.com>
2020-12-05 19:16:30 +0100

    day 4 part 2

--- src/day04.kt
+++ src/day04.kt
@@ -22,16 +22,97 @@ fun day04(input: List<String>): Int {
 
 fun validatePassports(passports: List<Passport>): Int {
     var count = 0
-    for (passport in passports){
+    for (passport in passports) {
         val fields = passport.fields
-        if(fields.count() >= 7){
-            if(fields.count() == 8 || !fields.containsKey("cid")){
-                count++
+        if (fields.count() >= 7) {
+            if (fields.count() == 8 || !fields.containsKey("cid")) {
+                if (validateValues(fields))
+                    count++
             }
         }
     }
     return count
 }
+
+fun validateValues(fields: Map<String, String>): Boolean {
+    val byr = fields.get("byr")
+    val iyr = fields.get("iyr")
+    val eyr = fields.get("eyr")
+    val hgt = fields.get("hgt")
+    val hcl = fields.get("hcl")
+    val ecl = fields.get("ecl")
+    val pid = fields.get("pid")
+    if (!validateRange(byr, 1920, 2002)) {
+        return false
+    } else if (!validateRange(iyr, 2010, 2020)) {
+        return false
+    } else if (!validateRange(eyr, 2020, 2030)) {
+        return false
+    } else if (!validateHgt(hgt)) {
+        return false
+    } else if (!validateHcl(hcl)) {
+        return false
+    } else if (!validateEcl(ecl)) {
+        return false
+    } else if (!validatePid(pid)) {
+        return false
+    }
+    return true
+}
+
+fun validatePid(pid: String?): Boolean {
+    val regex= Regex("^[0-9]{9}\$")
+    if(pid!!.matches(regex)){
+        return true
+    }
+    println(pid)
+    return false
+}
+
+fun validateHcl(hcl: String?): Boolean {
+    val regex= Regex("^#[0-9a-f]{6}\$")
+    if(hcl!!.matches(regex)){
+        return true
+    }
+    return false
+}
+
+fun validateEcl(ecl: String?): Boolean {
+    val regex= Regex("amb|blu|brn|gry|grn|hzl|oth")
+    if(ecl!!.matches(regex)){
+        return true
+    }
+    return false
+}
+
+fun validateHgt(hgt: String?): Boolean {
+    if (hgt != null) {
+        if (hgt.contains("in")) {
+            val hgtInches = hgt.split("i")
+            if (validateRange(hgtInches[0], 59, 76)) {
+                return true
+            }
+        } else if (hgt.contains("cm")) {
+            val hgtCentimeters = hgt.split("c")
+            if (validateRange(hgtCentimeters[0], 150, 193)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun validateRange(byr: String?, min: Int, max: Int): Boolean {
+    val byrAsInt = byr?.toInt()
+    if (byrAsInt != null) {
+        if (byrAsInt <= max && byrAsInt >= min) {
+            return true
+
+        }
+    }
+    return false
+}
+
 
 fun createPassport(template: StringBuilder): Passport {
     val passport: MutableMap<String, String> = mutableMapOf()
@@ -43,7 +124,6 @@ fun createPassport(template: StringBuilder): Passport {
             passport.put(field[0], field[1])
         }
     }
-    println(passport)
     return Passport(passport)
 }
 
